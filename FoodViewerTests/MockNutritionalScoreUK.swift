ad4c70c59495e7e37c45474fff4b42c55c78a3ab
Arnaud Leene <aleene@gmail.com>
2017-03-09 15:51:50 +0100

    Added (mocked) tests for NutritionalScoreUK

--- FoodViewerTests/MockNutritionalScoreUK.swift
+++ FoodViewerTests/MockNutritionalScoreUK.swift
@@ -1,14 +1,15 @@
 //
-//  NutritionalScoreUK.swift
+//  MockNutritionalScoreUK.swift
 //  FoodViewer
 //
-//  Created by arnaud on 09/06/16.
-//  Copyright © 2016 Hovering Above. All rights reserved.
+//  Created by arnaud on 09/03/17.
+//  Copyright © 2017 Hovering Above. All rights reserved.
 //
 
 import Foundation
+import FoodViewer
 
-public struct NutritionalScoreUK {
+public struct MockNutritionalScoreUK {
     public var pointsA = [nutrimentScore]()
     public var pointsC = [nutrimentScore]()
     public var score = 0
@@ -16,14 +17,14 @@ public struct NutritionalScoreUK {
     
     
     // conclusion
-    var total: Int {
+    public var total: Int {
         get {
             var sumA = 0
             for point in pointsA {
                 sumA += point.points
             }
             let sumC = pointsC[0].points + pointsC[1].points
-
+            
             if sumA < 11 {
                 return sumA - sumC - pointsC[2].points
             } else {
@@ -37,7 +38,7 @@ public struct NutritionalScoreUK {
             }
         }
     }
-
+    
     public struct nutrimentScore {
         public var nutriment = ""
         public var points = 0
@@ -59,37 +60,45 @@ public struct NutritionalScoreUK {
     }
     
     
-    fileprivate mutating func setup() {
-        let preferredLanguage = Locale.preferredLanguages[0]
-
+    private mutating func setup() {
+        
+        //MOCKED let preferredLanguage = Locale.preferredLanguages[0]
+        
         var energy = nutrimentScore()
-        energy.nutriment = OFFplists.manager.translateNutrients(Constants.energyKey, language:preferredLanguage)
+        //MOCKED energy.nutriment = OFFplists.manager.translateNutrients(Constants.energyKey, language:preferredLanguage)
+        energy.nutriment = Constants.energyKey
         energy.points = 0
         
         var saturatedFats = nutrimentScore()
-        saturatedFats.nutriment = OFFplists.manager.translateNutrients(Constants.saturatedFatKey, language:preferredLanguage)
+        //MOCKED saturatedFats.nutriment = OFFplists.manager.translateNutrients(Constants.saturatedFatKey, language:preferredLanguage)
+        saturatedFats.nutriment = Constants.saturatedFatKey
         saturatedFats.points = 0
-
+        
         var sugars = nutrimentScore()
-        sugars.nutriment = OFFplists.manager.translateNutrients(Constants.sugarsKey, language:preferredLanguage)
+        //MOCKED sugars.nutriment = OFFplists.manager.translateNutrients(Constants.sugarsKey, language:preferredLanguage)
+        sugars.nutriment = Constants.sugarsKey
         sugars.points = 0
         
         var sodium = nutrimentScore()
-        sodium.nutriment = OFFplists.manager.translateNutrients(Constants.sodiumKey, language:preferredLanguage)
+        //MOCKED sodium.nutriment = OFFplists.manager.translateNutrients(Constants.sodiumKey, language:preferredLanguage)
+        sodium.nutriment = Constants.sodiumKey
         sodium.points = 0
         
         pointsA = [energy, saturatedFats, sugars, sodium]
         
         var fruitsVegetablesNuts = nutrimentScore()
-        fruitsVegetablesNuts.nutriment = OFFplists.manager.translateNutrients(Constants.fruitsVegetablesNutsKey, language:preferredLanguage)
+        //MOCKED fruitsVegetablesNuts.nutriment = OFFplists.manager.translateNutrients(Constants.fruitsVegetablesNutsKey, language:preferredLanguage)
+        fruitsVegetablesNuts.nutriment = Constants.fruitsVegetablesNutsKey
         fruitsVegetablesNuts.points = 0
         
         var fiber = nutrimentScore()
-        fiber.nutriment = OFFplists.manager.translateNutrients(Constants.fiberKey, language:preferredLanguage)
+        //MOCKED fiber.nutriment = OFFplists.manager.translateNutrients(Constants.fiberKey, language:preferredLanguage)
+        fiber.nutriment = Constants.fiberKey
         fiber.points = 0
         
         var proteins = nutrimentScore()
-        proteins.nutriment = OFFplists.manager.translateNutrients(Constants.proteinsKey, language:preferredLanguage)
+        //MOCKED proteins.nutriment = OFFplists.manager.translateNutrients(Constants.proteinsKey, language:preferredLanguage)
+        proteins.nutriment = Constants.proteinsKey
         proteins.points = 0
         
         pointsC = [fruitsVegetablesNuts, fiber, proteins]
