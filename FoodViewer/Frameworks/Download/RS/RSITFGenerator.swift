6128310d8cf679bfc4658015be9023f79822b601
Arnaud Leene <aleene@gmail.com>
2020-02-15 16:38:53 +0100

    RS Framework

---
+++ FoodViewer/Frameworks/Download/RS/RSITFGenerator.swift
@@ -0,0 +1,67 @@
+//
+//  RSITFGenerator.swift
+//  RSBarcodesSample
+//
+//  Created by R0CKSTAR on 6/11/14.
+//  Copyright (c) 2014 P.D.Q. All rights reserved.
+//
+
+import UIKit
+
+// http://www.barcodeisland.com/int2of5.phtml
+open class RSITFGenerator: RSAbstractCodeGenerator {
+    let ITF_CHARACTER_ENCODINGS = [
+        "00110",
+        "10001",
+        "01001",
+        "11000",
+        "00101",
+        "10100",
+        "01100",
+        "00011",
+        "10010",
+        "01010",
+    ]
+    
+    override open func isValid(_ contents: String) -> Bool {
+        return super.isValid(contents) && contents.count % 2 == 0
+    }
+    
+    override open func initiator() -> String {
+        return "1010"
+    }
+    
+    override open func terminator() -> String {
+        return "1101"
+    }
+    
+    override open func barcode(_ contents: String) -> String {
+        var barcode = ""
+        for i in 0..<contents.count / 2 {
+            let pair = contents.substring(i * 2, length: 2)
+            if let pair = pair {
+                let bars = ITF_CHARACTER_ENCODINGS[Int(pair[0])!]
+                let spaces = ITF_CHARACTER_ENCODINGS[Int(pair[1])!]
+                
+                for j in 0..<10 {
+                    if j % 2 == 0 {
+                        let bar = Int(bars[j / 2])
+                        if bar == 1 {
+                            barcode += "11"
+                        } else {
+                            barcode += "1"
+                        }
+                    } else {
+                        let space = Int(spaces[j / 2])
+                        if space == 1 {
+                            barcode += "00"
+                        } else {
+                            barcode += "0"
+                        }
+                    }
+                }
+            }
+        }
+        return barcode
+    }
+}
---
+++ FoodViewer/Frameworks/Download/RS/RSUnifiedCodeGenerator.swift
@@ -0,0 +1,92 @@
+//
+//  RSUnifiedCodeGenerator.swift
+//  RSBarcodesSample
+//
+//  Created by R0CKSTAR on 6/10/14.
+//  Copyright (c) 2014 P.D.Q. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import AVFoundation
+
+open class RSUnifiedCodeGenerator: RSCodeGenerator {
+    
+    open var isBuiltInCode128GeneratorSelected = false
+    open var fillColor: UIColor = UIColor.white
+    open var strokeColor: UIColor = UIColor.black
+    
+    open class var shared: RSUnifiedCodeGenerator {
+        return UnifiedCodeGeneratorSharedInstance
+    }
+    
+    // MARK: RSCodeGenerator
+    
+    open func isValid(_ contents: String) -> Bool {
+        print("Use RSUnifiedCodeValidator.shared.isValid(contents:String, machineReadableCodeObjectType: String) instead")
+        return false
+    }
+    
+    open func generateCode(_ contents: String, inputCorrectionLevel: InputCorrectionLevel, machineReadableCodeObjectType: String) -> UIImage? {
+        var codeGenerator: RSCodeGenerator?
+        switch machineReadableCodeObjectType {
+        case AVMetadataObject.ObjectType.qr.rawValue, AVMetadataObject.ObjectType.pdf417.rawValue, AVMetadataObject.ObjectType.aztec.rawValue:
+            return RSAbstractCodeGenerator.generateCode(contents, inputCorrectionLevel: inputCorrectionLevel, filterName: RSAbstractCodeGenerator.filterName(machineReadableCodeObjectType))
+        case AVMetadataObject.ObjectType.code39.rawValue:
+            codeGenerator = RSCode39Generator()
+        case AVMetadataObject.ObjectType.code39Mod43.rawValue:
+            codeGenerator = RSCode39Mod43Generator()
+        case AVMetadataObject.ObjectType.ean8.rawValue:
+            codeGenerator = RSEAN8Generator()
+        case AVMetadataObject.ObjectType.ean13.rawValue:
+            codeGenerator = RSEAN13Generator()
+        case AVMetadataObject.ObjectType.interleaved2of5.rawValue:
+            codeGenerator = RSITFGenerator()
+        case AVMetadataObject.ObjectType.itf14.rawValue:
+            codeGenerator = RSITF14Generator()
+        case AVMetadataObject.ObjectType.upce.rawValue:
+            codeGenerator = RSUPCEGenerator()
+        case AVMetadataObject.ObjectType.code93.rawValue:
+            codeGenerator = RSCode93Generator()
+            // iOS 8 included, but my implementation's performance is better :)
+        case AVMetadataObject.ObjectType.code128.rawValue:
+            if self.isBuiltInCode128GeneratorSelected {
+                return RSAbstractCodeGenerator.generateCode(contents, inputCorrectionLevel: inputCorrectionLevel, filterName: RSAbstractCodeGenerator.filterName(machineReadableCodeObjectType))
+            } else {
+                codeGenerator = RSCode128Generator()
+            }
+        case AVMetadataObject.ObjectType.dataMatrix.rawValue:
+            codeGenerator = RSCodeDataMatrixGenerator()
+        case RSBarcodesTypeISBN13Code:
+            codeGenerator = RSISBN13Generator()
+        case RSBarcodesTypeISSN13Code:
+            codeGenerator = RSISSN13Generator()
+        case RSBarcodesTypeExtendedCode39Code:
+            codeGenerator = RSExtendedCode39Generator()
+        default:
+            print("No code generator selected.")
+        }
+        
+        if codeGenerator != nil {
+            codeGenerator!.fillColor = self.fillColor
+            codeGenerator!.strokeColor = self.strokeColor
+            return codeGenerator!.generateCode(contents, inputCorrectionLevel: inputCorrectionLevel, machineReadableCodeObjectType: machineReadableCodeObjectType)
+        } else {
+            return nil
+        }
+    }
+    
+    open func generateCode(_ contents: String, machineReadableCodeObjectType: String) -> UIImage? {
+        return self.generateCode(contents, inputCorrectionLevel: .Medium, machineReadableCodeObjectType: machineReadableCodeObjectType)
+    }
+    
+    open func generateCode(_ machineReadableCodeObject: AVMetadataMachineReadableCodeObject, inputCorrectionLevel: InputCorrectionLevel) -> UIImage? {
+        return self.generateCode(machineReadableCodeObject.stringValue!, inputCorrectionLevel: inputCorrectionLevel, machineReadableCodeObjectType: machineReadableCodeObject.type.rawValue)
+    }
+    
+    open func generateCode(_ machineReadableCodeObject: AVMetadataMachineReadableCodeObject) -> UIImage? {
+        return self.generateCode(machineReadableCodeObject, inputCorrectionLevel: .Medium)
+    }
+}
+
+let UnifiedCodeGeneratorSharedInstance = RSUnifiedCodeGenerator()
